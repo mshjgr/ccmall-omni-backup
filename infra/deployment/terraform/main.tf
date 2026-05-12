4afeab4f4442b6e32d9af83677d3012f05fcc714
changsiuuuu <changsiuuuu@gmail.com>
2026-05-12 15:24:01 +0900

    refactor:terraform_data와 네임규칙수정

--- infra/deployment/terraform/main.tf
+++ infra/deployment/terraform/main.tf
@@ -17,15 +17,15 @@ provider "aws" {
 }
 
 # 2. vpc 및 네트워크 생성
-resource "aws_vpc" "main" {
+resource "aws_vpc" "ccmall_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags                 = { Name = "ccmall-vpc" }
 }
 
 # igw
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "ccmall_igw" {
+  vpc_id = aws_vpc.ccmall_vpc.id
   tags   = { Name = "ccmall-igw" }
 }
 
@@ -35,8 +35,8 @@ data "aws_availability_zones" "available" {
 }
 
 # public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "ccmall_public_subnet" {
+  vpc_id                  = aws_vpc.ccmall_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
@@ -47,8 +47,8 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # private subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "ccmall_private_subnet" {
+  vpc_id                  = aws_vpc.ccmall_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = false
@@ -59,13 +59,13 @@ resource "aws_subnet" "private_subnet" {
 }
 
 # public subnet 라우팅 테이블
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "ccmall_public_rt" {
+  vpc_id = aws_vpc.ccmall_vpc.id
 
   # public subnet에서 인터넷으로 나가는 트래픽은 IGW로 보낸다.
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.ccmall_igw.id
   }
 
   tags = {
@@ -75,12 +75,12 @@ resource "aws_route_table" "public_rt" {
 
 # public subnet과 public route table 연결
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.ccmall_public_subnet.id
+  route_table_id = aws_route_table.ccmall_public_rt.id
 }
 
 # NAT Gateway에 붙일 탄력적 IP 생성
-resource "aws_eip" "nat_eip" {
+resource "aws_eip" "ccmall_nat_eip" {
   domain = "vpc"
 
   tags = {
@@ -89,11 +89,11 @@ resource "aws_eip" "nat_eip" {
 }
 
 # NAT Gateway 생성
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+resource "aws_nat_gateway" "ccmall_nat_gw" {
+  allocation_id = aws_eip.ccmall_nat_eip.id
+  subnet_id     = aws_subnet.ccmall_public_subnet.id
 
-  depends_on = [aws_internet_gateway.igw]
+  depends_on = [aws_internet_gateway.ccmall_igw]
 
   tags = {
     Name = "ccmall-nat-gw"
@@ -101,36 +101,36 @@ resource "aws_nat_gateway" "nat_gw" {
 }
 
 # private subnet 라우팅 테이블
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "ccmall_private_rt" {
+  vpc_id = aws_vpc.ccmall_vpc.id
 
   # private subnet에서 외부 인터넷으로 나가는 트래픽은 NAT Gateway로 보낸다.
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
+    nat_gateway_id = aws_nat_gateway.ccmall_nat_gw.id
   }
 
   tags = {
-    Name = "ccmall-private-rt"
+    Name = "ccmall-private-nat"
   }
 }
 
 # private subnet과 private route table 연결
 resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.ccmall_private_subnet.id
+  route_table_id = aws_route_table.ccmall_private_rt.id
 }
 
 # pem 파일 관련 작업
-resource "tls_private_key" "pk" {
+resource "tls_private_key" "ccmall_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 # AWS Key Pair 등록
-resource "aws_key_pair" "kp" {
+resource "aws_key_pair" "ccmall_key" {
   key_name   = "ccmall-key"
-  public_key = tls_private_key.pk.public_key_openssh
+  public_key = tls_private_key.ccmall_private_key.public_key_openssh
 
   tags = {
     Name = "ccmall-key"
@@ -138,16 +138,16 @@ resource "aws_key_pair" "kp" {
 }
 
 # Terraform이 생성하는 초기 접속용 개인키
-resource "local_file" "ssh_key" {
-  filename        = local.ssh_key_file
-  content         = tls_private_key.pk.private_key_pem
+resource "local_file" "ccmall_ssh_key" {
+  filename        = local.ccmall_ssh_key_file
+  content         = tls_private_key.ccmall_private_key.private_key_pem
   file_permission = "0600"
 }
 
 # web 서버 보안 그룹
 resource "aws_security_group" "sg_web" {
   name   = "SG-Web"
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.ccmall_vpc.id
 
   # SSH
   # 실습 단계에서는 0.0.0.0/0으로 열고, 운영에서는 본인 공인 IP/32로 제한한다.
@@ -198,7 +198,7 @@ resource "aws_security_group" "sg_web" {
 # rec 서버 보안 그룹
 resource "aws_security_group" "sg_rec" {
   name   = "SG-Rec"
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.ccmall_vpc.id
 
   # EC2-Web에서 EC2-Rec으로 SSH 허용
   ingress {
@@ -254,12 +254,12 @@ resource "aws_security_group" "sg_rec" {
 }
 
 # s3 버킷 및 IAM 설정
-resource "random_id" "bucket_suffix" {
+resource "random_id" "ccmall_bucket_suffix" {
   byte_length = 2
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "ccmall-bucket-${random_id.bucket_suffix.hex}"
+resource "aws_s3_bucket" "ccmall_bucket" {
+  bucket = "ccmall-bucket-${random_id.ccmall_bucket_suffix.hex}"
 }
 
 # EC2가 S3에 접근할 IAM Role
@@ -298,14 +298,14 @@ data "aws_ami" "latest_al2023" {
 }
 
 # web ec2 만들기
-resource "aws_instance" "ec2_web" {
+resource "aws_instance" "ccmall_web" {
   ami                         = data.aws_ami.latest_al2023.id
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
+  subnet_id                   = aws_subnet.ccmall_public_subnet.id
   private_ip                  = "10.0.1.10"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg_web.id]
-  key_name                    = aws_key_pair.kp.key_name
+  key_name                    = aws_key_pair.ccmall_key.key_name
 
   root_block_device {
     volume_size           = 8
@@ -320,23 +320,6 @@ resource "aws_instance" "ec2_web" {
 
     # /etc/hosts 에도 일관성 있게 반영
     grep -q "127.0.0.1 Web" /etc/hosts || echo "127.0.0.1 Web" >> /etc/hosts
-
-    # user1 생성
-    id user1 >/dev/null 2>&1 || useradd -m -s /bin/bash user1
-
-    # wheel 그룹에 추가
-    usermod -aG wheel user1
-
-    # sudo NOPASSWD 설정
-    echo "user1 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user1
-    chmod 0440 /etc/sudoers.d/user1
-
-    # SSH 공개키 등록
-    mkdir -p /home/user1/.ssh
-    echo "${file("/home/user1/.ssh/ansiblekey.pem.pub")}" > /home/user1/.ssh/authorized_keys
-    chown -R user1:user1 /home/user1/.ssh
-    chmod 700 /home/user1/.ssh
-    chmod 600 /home/user1/.ssh/authorized_keys
   EOF
 
   tags = {
@@ -345,14 +328,14 @@ resource "aws_instance" "ec2_web" {
 }
 
 # rec ec2 만들기
-resource "aws_instance" "ec2_rec" {
+resource "aws_instance" "ccmall_rec" {
   ami                         = data.aws_ami.latest_al2023.id
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private_subnet.id
+  subnet_id                   = aws_subnet.ccmall_private_subnet.id
   private_ip                  = "10.0.2.30"
   associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.sg_rec.id]
-  key_name                    = aws_key_pair.kp.key_name
+  key_name                    = aws_key_pair.ccmall_key.key_name
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
@@ -369,22 +352,6 @@ resource "aws_instance" "ec2_rec" {
     # /etc/hosts 에도 일관성 있게 반영
     grep -q "127.0.0.1 Rec" /etc/hosts || echo "127.0.0.1 Rec" >> /etc/hosts
 
-    # user1 생성
-    id user1 >/dev/null 2>&1 || useradd -m -s /bin/bash user1
-
-    # wheel 그룹에 추가
-    usermod -aG wheel user1
-
-    # sudo NOPASSWD 설정
-    echo "user1 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/user1
-    chmod 0440 /etc/sudoers.d/user1
-
-    # SSH 공개키 등록
-    mkdir -p /home/user1/.ssh
-    echo "${file("/home/user1/.ssh/ansiblekey.pem.pub")}" > /home/user1/.ssh/authorized_keys
-    chown -R user1:user1 /home/user1/.ssh
-    chmod 700 /home/user1/.ssh
-    chmod 600 /home/user1/.ssh/authorized_keys
   EOF
   tags = {
     Name = "ccmall-Rec"
@@ -394,25 +361,25 @@ resource "aws_instance" "ec2_rec" {
 # 생성된 EC2-Web의 public ip를 출력
 output "web_public_ip" {
   description = "EC2-Web의 public ipv4 주소"
-  value       = aws_instance.ec2_web.public_ip
+  value       = aws_instance.ccmall_web.public_ip
 }
 
 # 생성된 EC2-Web의 private ip를 출력
 output "web_private_ip" {
   description = "EC2-Web의 private ipv4 주소"
-  value       = aws_instance.ec2_web.private_ip
+  value       = aws_instance.ccmall_web.private_ip
 }
 
 # 생성된 EC2-Rec의 private ip를 출력
 output "rec_private_ip" {
   description = "EC2-Rec의 private ipv4 주소"
-  value       = aws_instance.ec2_rec.private_ip
+  value       = aws_instance.ccmall_rec.private_ip
 }
 
 # 생성된 s3의 버킷 이름 출력
 output "s3_bucket_name" {
   description = "생성된 s3 버킷의 이름"
-  value       = aws_s3_bucket.my_bucket.id
+  value       = aws_s3_bucket.ccmall_bucket.id
 }
 locals {
   # 현재 Terraform 코드가 있는 위치
@@ -441,7 +408,7 @@ locals {
   recovery_roles_dir   = "${local.infra_dir}/recovery/ansible/roles"
 
   # Terraform이 생성하는 초기 접속용 key
-  ssh_key_file = "${local.terraform_dir}/ccmall-key.pem"
+  ccmall_ssh_key_file = "${local.terraform_dir}/ccmall-key.pem"
 
   # 부트스트랩 이후 운영 접속용 key
   ansible_key_file = "/home/user1/.ssh/ansiblekey.pem"
@@ -471,19 +438,19 @@ resource "local_file" "ansible_inventory" {
   content = yamlencode({
     all = {
       hosts = {
-        # EC2-Web은 public subnet에 있으므로 public ip로 직접 접속한다.
-        "EC2-Web" = {
-          ansible_host = aws_instance.ec2_web.public_ip
+        # ccmall-Web은 public subnet에 있으므로 public ip로 직접 접속한다.
+        "ccmall-Web" = {
+          ansible_host = aws_instance.ccmall_web.public_ip
         }
 
-        # EC2-Rec은 private subnet에 있으므로 EC2-Web을 통해 점프 접속한다.
-        "EC2-Rec" = {
-          ansible_host = aws_instance.ec2_rec.private_ip
+        # ccmall-Rec은 private subnet에 있으므로 ccmall-Web을 통해 점프 접속한다.
+        "ccmall-Rec" = {
+          ansible_host = aws_instance.ccmall_rec.private_ip
 
           # %r은 현재 Ansible 접속 사용자로 치환된다.
           # 부트스트랩 때는 ec2-user,
           # 운영 때는 user1로 동작한다.
-          ansible_ssh_common_args = "-o ProxyJump=%r@${aws_instance.ec2_web.public_ip} -o StrictHostKeyChecking=no"
+          ansible_ssh_common_args = "-o ProxyCommand=\"ssh -i ${local.ccmall_ssh_key_file} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p -q %r@${aws_instance.ccmall_web.public_ip}\" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         }
 
         # rocky01은 VMware에 있는 Rocky Linux 기반 On-Prem DB 서버다.
@@ -513,22 +480,50 @@ resource "local_file" "ansible_cfg" {
   EOF
 }
 
+# =============================================
+# Terraform → Ansible user1 부트스트랩 자동실행
+# EC2 생성 + SSH Key + inventory + ansible.cfg 준비 후 실행
+# ec2_bootstrap.yml 실행으로 user1 생성 및 SSH 접속 준비
+# =============================================
 resource "terraform_data" "bootstrap_user1" {
+
   depends_on = [
-    aws_instance.ec2_web,
-    aws_instance.ec2_rec,
-    local_file.ssh_key,
-    local_file.ansible_inventory,
-    local_file.ansible_cfg
+    aws_instance.ccmall_web,          # Web 서버 생성 완료 후
+    aws_instance.ccmall_rec,          # Rec 서버 생성 완료 후
+    local_file.ccmall_ssh_key,            # SSH Private Key 생성 완료 후
+    local_file.ansible_inventory,  # inventory.yml 생성 완료 후
+    local_file.ansible_cfg         # ansible.cfg 생성 완료 후
   ]
 
   triggers_replace = {
-    web_instance_id = aws_instance.ec2_web.id
-    rec_instance_id = aws_instance.ec2_rec.id
+    web_instance_id = aws_instance.ccmall_web.id
+    rec_instance_id = aws_instance.ccmall_rec.id
   }
 
   provisioner "local-exec" {
-    command = "sleep 60 && ANSIBLE_CONFIG=${local.ansible_cfg} ANSIBLE_SSH_PIPELINING=1 ansible-playbook -u ec2-user --private-key ${local.ssh_key_file} ${local.bootstrap_playbook}"
+    # ansible.cfg 기준 실행 위치
+    working_dir = local.infra_dir
+
+    command = <<-EOT
+      echo "======================================"
+      echo " EC2 SSH 준비 대기 중... (40초)"
+      echo "======================================"
+      sleep 40
+
+      echo "======================================"
+      echo " Ansible Bootstrap Playbook 시작!"
+      echo "======================================"
+      ANSIBLE_CONFIG=${local.ansible_cfg} \
+      ANSIBLE_SSH_PIPELINING=1 \
+      ansible-playbook \
+        -u ec2-user \
+        --private-key ${local.ccmall_ssh_key_file} \
+        ${local.bootstrap_playbook}
+
+      echo "======================================"
+      echo " Bootstrap Playbook 완료!"
+      echo "======================================"
+    EOT
   }
 }
 
@@ -540,38 +535,38 @@ resource "terraform_data" "bootstrap_user1" {
 resource "terraform_data" "run_monitoring_playbook" {
 
   depends_on = [
-    aws_instance.ec2_web,          # Web 서버 생성 완료 후
-    aws_instance.ec2_rec,          # Rec 서버 생성 완료 후
+    aws_instance.ccmall_web,          # Web 서버 생성 완료 후
+    aws_instance.ccmall_rec,          # Rec 서버 생성 완료 후
     local_file.ansible_inventory,  # inventory.yml 생성 완료 후
     local_file.ansible_cfg,        # ansible.cfg 생성 완료 후
     terraform_data.bootstrap_user1 # bootstrap 완료 후
   ]
 
   triggers_replace = {
-    web_instance_id = aws_instance.ec2_web.id
-    rec_instance_id = aws_instance.ec2_rec.id
+    web_instance_id = aws_instance.ccmall_web.id
+    rec_instance_id = aws_instance.ccmall_rec.id
     bootstrap_id    = terraform_data.bootstrap_user1.id
   }
 
   provisioner "local-exec" {
-    # ansible.cfg 가 있는 infra/ 폴더에서 실행
+    # ansible.cfg 기준 실행 위치
     working_dir = local.infra_dir
 
     command = <<-EOT
       echo "======================================"
-      echo " EC2 SSH 준비 대기 중... (60초)"
+      echo " EC2 SSH 준비 대기 중... (10초)"
       echo "======================================"
-      sleep 60
+      sleep 10
 
       echo "======================================"
-      echo " Ansible 모니터링 Playbook 시작!"
+      echo " Ansible Monitoring Playbook 시작!"
       echo "======================================"
       ANSIBLE_CONFIG=${local.ansible_cfg} \
       ANSIBLE_SSH_PIPELINING=1 \
       ansible-playbook monitoring/playbook.yml
 
       echo "======================================"
-      echo " Playbook 완료!"
+      echo " Monitoring Playbook 완료!"
       echo "======================================"
     EOT
   }
